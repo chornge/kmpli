a0a4dc75b96cb556bf6965a290abb259db472dd8
Christian <Mxlightningx@gmail.com>
2025-12-16 01:06:06 -0600

    Update to v1.2.6

--- Formula/kmpli.rb
+++ Formula/kmpli.rb
@@ -1,7 +1,7 @@
 class Kmpli < Formula
   desc "CLI tool for generating Kotlin Multiplatform projects"
   homepage "https://github.com/chornge/kmpli"
-  version "1.2.5"
+  version "1.2.6"
   license "Apache-2.0"
 
   on_macos do
