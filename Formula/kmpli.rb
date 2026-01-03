52e8cc96fab27759eddc76d84bc344bc16d7dcf4
Christian Mbaba <christianmbaba@live.com>
2026-01-02 23:46:30 -0600

    Update package manifests for v1.2.11 (#16)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/kmpli.rb
+++ Formula/kmpli.rb
@@ -1,21 +1,21 @@
 class Kmpli < Formula
   desc "CLI tool for generating Kotlin Multiplatform projects"
   homepage "https://github.com/chornge/kmpli"
-  version "1.2.10"
+  version "1.2.11"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/chornge/kmpli/releases/download/v1.2.10/kmpli-macos-arm64"
-      sha256 "12a7389f319e0a8bbeb3aa594e4468e067cfaacc5e96577bf499342bbf8d8f4e"
+      url "https://github.com/chornge/kmpli/releases/download/v1.2.11/kmpli-macos-arm64"
+      sha256 "30de0a3c365ae6a22936aa3a8bd63f42203f6ec229776e511e5180050eedc848"
     else
-      url "https://github.com/chornge/kmpli/releases/download/v1.2.10/kmpli-macos-x64"
-      sha256 "a3d227a33cc70d879bf1b4d461f8f30eb10f45128c29f7c20b0a9d72aa678852"
+      url "https://github.com/chornge/kmpli/releases/download/v1.2.11/kmpli-macos-x64"
+      sha256 "44b6b5b506726d5eb2822ab3f664ff4bcb9a379af113e145e747f7ead03eb529"
     end
   end
 
   on_linux do
-    url "https://github.com/chornge/kmpli/releases/download/v1.2.10/kmpli-linux-x64"
+    url "https://github.com/chornge/kmpli/releases/download/v1.2.11/kmpli-linux-x64"
     sha256 "ac04532899cf27b44bb5a4d4c609b0d459f33928ed274686d02994b6d3b9dc9d"
   end
 
